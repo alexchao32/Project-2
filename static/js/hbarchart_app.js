779e047dc4a2c0adffaa9cc94fd9403dd9d2e2b1
RimaSusan <susanrima@gmail.com>
2019-09-24 00:34:10 -0400

    updated the hbarchart route code

--- static/js/hbarchart_app.js
+++ static/js/hbarchart_app.js
@@ -8,64 +8,80 @@ var LifeExpectancy= []
 var Trust=[]
 var Dystopia=[]
 
+// $(document).ready(function(){
+//   $("#btn_2018").click(function() {
+//     $("#myChart").remove();
+    
+//     console.log("removed chart");
+//     $("#chartReport").append('<canvas id="myChart" class="canvasclass"></canvas>');
+//     loadfrom_getData(2018);
+//   });
+//  });
+
+//select the table body using d3
+//var canvas = d3.select("canvas");
+
+///:LOADING FROM CSV
+// function loadData(){
+//  d3.csv("static/js/2017.csv", function(csv) {
+
+//     for (x in csv) {
+//             if (x=="Country"){
+//                 Country.push(csv[x]);               
+//             }
+//             else if (x=="Economy..GDP.per.Capita."){
+//                 Economy.push(csv[x]);
+//             }
+//             else if (x=="Family"){
+//                 Family.push(csv[x]);
+//             }
+//             else if (x=="Freedom"){
+//                 Freedom.push(csv[x]);
+//             }
+//             else if (x=="Generosity"){
+//                 Generosity.push(csv[x]);
+//             }
+//             else if (x=="Health..Life.Expectancy."){
+//                 LifeExpectancy.push(csv[x]);
+//             }
+//             else if (x=="Trust..Government.Corruption."){
+//                 Trust.push(csv[x]);
+//             }
+//             else if (x=="Dystopia.Residual"){
+//                 Dystopia.push(csv[x]);
+//             }
+//             //console.log(x);
+//             //console.log(csv[x]);
+//         //if (key='Country'){
+//           //  Country.push(csv[key])
 
 
+//         }
+//        // console.log(key, csv[key]);
 
-
-function loadData(){
- d3.csv("static/js/2017.csv", function(csv) {
-
-    for (x in csv) {
-            if (x=="Country"){
-                Country.push(csv[x]);               
-            }
-            else if (x=="Economy..GDP.per.Capita."){
-                Economy.push(csv[x]);
-            }
-            else if (x=="Family"){
-                Family.push(csv[x]);
-            }
-            else if (x=="Freedom"){
-                Freedom.push(csv[x]);
-            }
-            else if (x=="Generosity"){
-                Generosity.push(csv[x]);
-            }
-            else if (x=="Health..Life.Expectancy."){
-                LifeExpectancy.push(csv[x]);
-            }
-            else if (x=="Trust..Government.Corruption."){
-                Trust.push(csv[x]);
-            }
-            else if (x=="Dystopia.Residual"){
-                Dystopia.push(csv[x]);
-            }
-            //console.log(x);
-            //console.log(csv[x]);
-        //if (key='Country'){
-          //  Country.push(csv[key])
-
-
-        }
-       // console.log(key, csv[key]);
-
-      }
-    //console.log(csv)
-    //csv.map(function(d) {
-      //  Dystopia.push(d.Dystopia.Residual);
-        //prevalence.push(d.prevalence);
-        //disability.push(d.disability);
+//       }
+//     //console.log(csv)
+//     //csv.map(function(d) {
+//       //  Dystopia.push(d.Dystopia.Residual);
+//         //prevalence.push(d.prevalence);
+//         //disability.push(d.disability);
    
-    //})
-    //console.log(data);
-);
+//     //})
+//     //console.log(data);
+// );
 
-    };
+//     };
 
 
 
 function loadChart() {
-var stackedBar = new Chart(document.getElementById('myChart'), {
+
+//  jQuery(".canvasclass").remove;
+//  jQuery(".card-body chart-container").append('<canvas id="myChart" class="jQuery"></canvas>');
+
+//canvas.html("");
+
+stackedBar = new Chart(document.getElementById('myChart'), {
   type: 'horizontalBar',
   data: {
     labels: Country,
@@ -116,10 +132,17 @@ var stackedBar = new Chart(document.getElementById('myChart'), {
       scales: {
           xAxes: [{
               stacked: true,
-              fontSize: 50
+              gridLines: {
+                display: false
+              },
+              fontSize: 50,
+              position: 'top'
           }],
           yAxes: [{
               stacked: true,
+              gridLines: {
+                display: false
+              },
               barPercentage: 0.8,
               //barThickness: 50
               categoryPercentage: 0.7
@@ -131,9 +154,10 @@ var stackedBar = new Chart(document.getElementById('myChart'), {
 
 };
 
+//LOADING from json
 
 function loadfrom_getData(year) {
-  console.log("Last Linexxxx");
+  console.log("inside fucntion loadfrom_getData");
 
   var getdataURL = "/getdata/" + String(year);
   d3.json(getdataURL, function (data) {
@@ -143,18 +167,52 @@ function loadfrom_getData(year) {
   //tableData=data;
 
   Object.entries(data).forEach(function([key, value]) {
-    console.log(key, value);
+    //console.log("inside loadfrom")
+    //console.log(Object.keys(value));
 
+    for (var x in value){
+      //console.log("x_key="+x);
+      //console.log("data="+value[x]);
+
+        if (x=="Country"){
+          Country.push(value[x]);               
+      }
+      else if (x=="EconomyGDPperCapita"){
+          Economy.push(value[x]);
+      }
+      else if (x=="Family"){
+          Family.push(value[x]);
+      }
+      else if (x=="Freedom"){
+          Freedom.push(value[x]);
+      }
+      else if (x=="Generosity"){
+          Generosity.push(value[x]);
+      }
+      else if (x=="HealthLifeExpectancy"){
+          LifeExpectancy.push(value[x]);
+      }
+      else if (x=="TrustGovernmentCorruption"){
+          Trust.push(value[x]);
+      }
+      else if (x=="DystopiaResidual"){
+          Dystopia.push(value[x]);
+    }
+    }
     });
-    console.log("Last Linexxxxyyyyy");
-    loadData();
+    //console.log("Last Line after if loop");
+    //console.log("country[]="+Country);
+    //console.log("GDP[]="+Economy)
+    //loadData();
+    
     loadChart();
 })
+
 }
- 
 
 
-loadfrom_getData(2015);
+
+loadfrom_getData(2018);
 console.log("Last Line");
 //loadData();
 //loadChart();
